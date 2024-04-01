58aa120264dfc340d7fe75a5b39de2bb9173bae7
satriaranggaj <satriarangga43@gmail.com>
2024-04-02 05:52:22 +0700

    fix testimoni

--- resources/js/Components/Home/Testimoni.vue
+++ resources/js/Components/Home/Testimoni.vue
@@ -28,7 +28,7 @@ const props = defineProps({
                         <n-image
                             style="margin: 10px;"
                             width="400"
-                            :src="route('home.index') + '/' + testimoniItem.image"
+                            :src="testimoniItem.image"
                             v-for="(testimoniItem, index) in testimoni" :key="`testimoni-${index}`"
                         />
                     </n-space>
